4e73c39d0ceb52d85a519ff80559a2ef343e3a65
Juan Sánchez-Ocaña <juan_sanchez@hotmail.com>
2020-06-15 03:19:20 +0200

    cosas

--- maps.js
+++ maps.js
@@ -16,8 +16,6 @@ class Location {
       } else {
           $("#info").text("Este buscador no ofrece soporte para geolocalizacion.");
       }
-      
-      this.resizeMap();
   }
   showPosition(position) {
       var pos = {
@@ -27,6 +25,23 @@ class Location {
 
       new google.maps.Marker({position: pos, map: this.map});
 
+       // Crear marker por cada restaurante
+      let arr = [
+        {x: 43.3833306, y: -5.6666694444444445},
+        {x: 43.2916, y: -5.699380555555556},
+        {x:43.3602889, y: -5.844761111111111},
+        {x:43.4333306, y: -5.883330555555556},
+        {x:43.5293111, y: -5.677319444444445},
+        {x:43.5666694, y: -5.933330555555556}
+      ];
+      
+      arr.forEach(rest => {
+        new google.maps.Marker({
+          position: {lat: rest.x, lng: rest.y},
+          map: this.map,
+          icon: "https://uo252376.github.io/SEW_JUNIO_2020/resources/blue-dot.png"
+        });
+      });
 
       this.map.setCenter(pos);
   }
