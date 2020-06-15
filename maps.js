299e22fb9facb14a8f9056d6ccc5868ff721385e
Juan Sánchez-Ocaña <juan_sanchez@hotmail.com>
2020-06-15 03:05:59 +0200

    changes in maps

--- maps.js
+++ maps.js
@@ -9,10 +9,10 @@ class Location {
           center: {lat: -34.397, lng: 150.644},
           zoom: 6
       });
-      this.loadRestaurants();   
       this.infoWindow = new google.maps.InfoWindow;
       if(navigator.geolocation){
           navigator.geolocation.getCurrentPosition(this.showPosition.bind(this), this.showError.bind(this));
+          this.infoWindow.open(this.map);
       } else {
           $("#info").text("Este buscador no ofrece soporte para geolocalizacion.");
       }
@@ -23,7 +23,26 @@ class Location {
           lng: position.coords.longitude
       };
 
-      var marker = new google.maps.Marker({position: pos, map: this.map});
+      new google.maps.Marker({position: pos, map: this.map});
+
+       // Crear marker por cada restaurante
+      let arr = [
+        {x: 43.3833306, y: -5.6666694444444445},
+        {x: 43.2916, y: -5.699380555555556},
+        {x:43.3602889, y: -5.844761111111111},
+        {x:43.4333306, y: -5.883330555555556},
+        {x:43.5293111, y: -5.677319444444445},
+        {x:43.5666694, y: -5.933330555555556}
+      ];
+      
+      arr.forEach(rest => {
+        new google.maps.Marker({
+          position: {lat: rest.x, lng: rest.y},
+          map: this.map,
+          icon: "https://uo252376.github.io/SEW_JUNIO_2020/resources/blue-dot.png"
+        });
+      });
+
       this.map.setCenter(pos);
   }
 
@@ -47,26 +66,6 @@ class Location {
   resizeMap() {
     google.maps.event.trigger(this.map,'resize');
     this.map.setZoom( this.map.getZoom() );
-  }
-
-  loadRestaurants(){
-    // Crear marker por cada restaurante
-    let arr = [
-      {x: 43.3833306, y: -5.6666694444444445},
-      {x: 43.2916, y: -5.699380555555556},
-      {x:43.3602889, y: -5.844761111111111},
-      {x:43.4333306, y: -5.883330555555556},
-      {x:43.5293111, y: -5.677319444444445},
-      {x:43.5666694, y: -5.933330555555556}
-    ];
-    
-    arr.forEach(rest => {
-      new google.maps.Marker({
-        map: this.map,
-        position: {lat: rest.x, lng: rest.y},
-        icon: "https://uo252376.github.io/SEW_JUNIO_2020/resources/blue-dot.png"
-      });
-    });
   }
 
 }
